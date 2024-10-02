1a99f30a2a553b4ad0faa2f5f0bacee20745cce6
zgyj4532 <138000358+zgyj4532@users.noreply.github.com>
2024-10-02 21:00:47 +0800

    delete complex

--- exp1/main.cpp
+++ exp1/main.cpp
@@ -1,49 +1,7 @@
 #include <iostream>
-#include <complex>
-#include <random>
-#include <ctime>
-#include<iomanip>
 #include "Vector.cpp"
-#define fs2 fixed<<setprecision(2)<< //保留2位小数
+#include "Complex.cpp"
 using namespace std;
-// 复数类
-class Complex
-{
-private:
-    double real; // 实部
-    double imag; // 虚部
-public:
-    Complex(double r = 0.0, double i = 0.0) : real(r), imag(i) {}
-    double getr() const
-    {
-        return real;
-    }
-    double geti() const
-    {
-        return imag;
-    }
-    void print() const
-    {
-        if (imag == 0)
-            cout << fs2 real<< " ";
-        else
-            cout << fs2 (imag >= 0 ? "+" : "") << fs2 imag << "i "; // 三目运算符检测虚部正负号
-    }
-};
-Complex randomcomp(double min, double max)
-{
-
-    static default_random_engine generator; // 导入random_engine
-    generator.seed(random_device{}());
-    static uniform_real_distribution<double> distribution(min, max);
-    double real = distribution(generator);
-    // double imag = 0.0;
-    double imag = distribution(generator);
-    return Complex(real, imag);
-}
-
-    
-
 
 int main()
 {
@@ -55,19 +13,32 @@ int main()
         cv.insert(rc);
     }
     cout << "The original complex vector is :" << endl;
-    for(int i = 0 ;i <10 ; i++)
+    for (int i = 0; i < 10; i++)
     {
         cv[i].print();
     }
-    cout<<endl;
+    cout << endl;
     // 置乱
-    cv.unsort();
+    Vector<Complex> uncv =cv;
+    uncv.unsort();
     cout << "And then unsort complex vector is :" << endl;
-    for(int i = 0 ;i <10 ; i++)
+    for (int i = 0; i < 10; i++)
     {
-        cv[i].print();
+        uncv[i].print();
     }
-    //查找
-    cv.search()
+    // 查找
+    Vector<Complex> scv = cv;
+    for (int i = 0; i < 10; i++)
+    {
+        scv[i].same();//实现实部与虚部均相同
+    }
+    Complex aim_elem = scv[2];
+    cout << "The element to look for is;"<<endl;
+    aim_elem.print();
+    cout<<endl;
+    scv.search(aim_elem);
+
+    
+    
     return 0;
 }
