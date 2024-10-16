1f95dc0f4298badb3e8be2b73be0dd3f301b1704
zgyj4532 <2956918266@qq.com>
2024-10-16 20:19:00 +0800

    forget to delete the comment(

--- exp2/main.cpp
+++ exp2/main.cpp
@@ -6,16 +6,6 @@ int main()
     cout << "请输入算式：" << endl;
     cin >> equation;
     char *RPN = nullptr;
-    // op.push('\0');
-    // op.push('b');
-    // op.push('c');
-    // op.push('d');
-    // cout <<op.top()<<endl;
-    // char a = op.pop();
-    // char b = op.pop();
-    // char c = op.pop();
-    // char d = op.pop();
-    // cout <<a<<b<<c<<d<<op.pop()<<endl;
     cout << "计算结果为：" << evaluate(equation, RPN) << endl;
 
     return 0;
