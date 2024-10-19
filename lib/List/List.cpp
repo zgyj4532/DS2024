eeb8fe9d4f949a5807f617262d3ab4f22dc1f95d
zgyj4532 <2956918266@qq.com>
2024-10-19 18:57:53 +0800

    add copy and remove

--- lib/List/List.cpp
+++ lib/List/List.cpp
@@ -39,6 +39,7 @@ ttt ListNodePosi(T) List<T>::insertB(ListNodePosi(T) p,T const &e)
     _size++;
     return p->insertAsPred(e);
 }
+//插入
 ttt
 ListNodePosi(T) ListNode<T>::insertAsPred(T const& e)
 {
@@ -52,4 +53,41 @@ ListNodePosi(T) ListNode<T>::insertAsSucc(T const& e)
     ListNodePosi(T) x = new ListNode(e,this,succ);
     succ ->pred = x;succ = x;
     return x;
+}
+//基于复制的构造
+ttt
+void List<T>::copyNodes(ListNodePosi(T) p,int n)
+{
+    init();
+    while(n--) {insertAsLast(p->data);p=p->succ;}
+}
+//复制从p起的n项 相当于py中list=list[3:]
+ttt
+List<T>::List(ListNodePosi(T) p ,int n){copyNodes(p,n);}
+//整体复制
+ttt
+List<T>::List(List<T> const&L){copyNodes(L.first(),L._size);}
+//局部复制
+ttt
+List<T>::List(List<T> const&L,int r,int n){copyNodes(L[r],n);}
+//删除
+ttt T List<T>::remove(ListNodePosi(T) p)
+{
+    T e = p->data;
+    p->pred->succ = p->succ;p->succ->pred = p->pred;
+    delete p;
+    _size--;
+    return e;
+}
+//析构
+ttt List<T>::~List() {clear();delete header; delete trailer;}
+ttt int List<T>::clear()
+{
+    int olds = _size;
+    while (0<_size)
+    {
+        remove(header->succ);
+    }
+    return olds;
+    
 }
