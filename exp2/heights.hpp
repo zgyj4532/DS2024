3490ad2609eecd52929bfe1e6e6ea30cf8af6172
zgyj4532 <2956918266@qq.com>
2024-10-26 11:06:24 +0800

    update maxArea

--- exp2/heights.hpp
+++ exp2/heights.hpp
@@ -1,36 +1,58 @@
 #include "Stack.hpp"
 #define testConut 10
-//using monotonic stack 
+//使用动态规划
 int maxArea(Vector<int> &heights)
 {
-    Stack<int> stk;
-    int maxArea = 0;
-    // heights.insert(0);
-    for (int i = 0; i < heights.size(); ++i)
-    {
-        while (!stk.empty() && heights[stk.top()] > heights[i])
+
+    int n = heights.size();
+    if(n==0) return 0;
+    //example 2,1,5,6,2,3
+    Vector<int> leftmin(n);//height中位置i左侧最近的元素小于该柱子的位置
+    Vector<int> rightmin(n);//height中位置i右侧最近的元素小于该柱子的位置
+
+    leftmin[0] = -1;
+    for(int i=1;i<n;++i){
+        int t=i-1;
+        while (t>= 0 && heights[t] >= heights[i])
         {
-            int h = heights[stk.pop()];
-            // stk.pop();
-            int w = stk.empty() ? i : i - stk.top() - 1;
-            maxArea = max(maxArea, h * w);
+            t =leftmin[t];
         }
-        stk.push(i);
+        leftmin[i] = t;
+        //cout<<leftmin[i-1]<<endl;
+    }//循环结束后leftmin值为 -1,-1，1，2，1，4
+        rightmin[n-1] = n;
+    for(int i=n-2;i>=0;--i){
+        int t=i+1;
+        while (t<n && heights[t] >= heights[i])
+        {
+            t =rightmin[t];
+        }
+        rightmin[i] = t;
+        // cout<<rightmin[i]<<endl;
+    }//循环结束后rightmin值为1,6,4,4,6,6 (6等效于leftmin中的-1为找到)
+
+    int maxArea = 0;
+    for(int i=0;i<n;++i)
+    {
+        int h=heights[i];
+        int w=rightmin[i]-leftmin[i]-1;
+        maxArea = max(maxArea,h*w);
     }
+
     return maxArea;
 }
 void rand_test(int n)
 {
+    // int a[n]={2,1,5,6,2,3};
     Vector<int> heights(n,n,0);
-    for (int i = 0; i < n; i++)
-        heights[i] = rand() % 105; // 生成0，104范围内的随机数
-        //heights[i] = rand() % 10;
-    int area = maxArea(heights);
+    
     cout << "heights:[";
     for (int i = 0; i < n - 1; i++)
     {
+        heights[i] = rand() % 10;// 生成0，104范围内的随机数
         cout << heights[i] << ',';
     }
+    int area = maxArea(heights);
     cout << heights[n - 1] << ']' << endl;
     cout << area << endl;
 }
