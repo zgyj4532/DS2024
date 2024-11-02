fee947ef3b19027fb1e46791e6fb92312ad04c18
zgyj4532 <2956918266@qq.com>
2024-11-02 14:48:28 +0800

    update binmap

--- lib/Bintree/Binmap.hpp
+++ lib/Bintree/Binmap.hpp
@@ -14,17 +14,65 @@ protected:
         memset(M, 0, N);
         _sz = 0;
     }
+
 public:
-    Bitmap(Rank n = 8 ){init(n);}
-    Bitmap(char* file,Rank n =8)
+    Bitmap(Rank n = 8) { init(n); }
+    Bitmap(char *file, Rank n = 8)
     {
         init(n);
-        FILE* fp = fopen(file,"r");
-        fread(M,sizeof(char),N,fp);
+        FILE *fp = fopen(file, "r");
+        fread(M, sizeof(char), N, fp);
         fclose(fp);
-        for(Rank k = 0,_sz = 0;k<n;k++) _sz+=test(k);
+        for (Rank k = 0, _sz = 0; k < n; k++)
+            _sz += test(k);
     }
-    void expand(Rank& k){k*=2;}
-    bool test(Rank k){expand(k);return M[k>>3] &(0x80>>(k&0x07));}
-
+    ~Bitmap()
+    {
+        delete[] M;
+        M = NULL;
+        _sz = 0;
+    }
+    Rank size() { return _sz; }
+    void set(Rank k)
+    {
+        expand(k);
+        _sz++;
+        M[k >> 3] |= (0x80 >> (k & 0x07));
+    }
+    void clear(Rank k)
+    {
+        expand(k);
+        _sz--;
+        M[k >> 3] &= (0x80 >> (k & 0x07));
+    }
+    bool test(Rank k)
+    {
+        expand(k);
+        return M[k >> 3] & (0x80 >> (k & 0x07));
+    }
+    void dump(char *file)
+    {
+        FILE *fp = fopen(file, "w");
+        fwrite(M, sizeof(char), N, fp);
+        fclose(fp);
+    }
+    char *bits2string(Rank n)
+    {                  // 将前 n 位转换为字符串——
+        expand(n - 1); // 此时可能被访问的最高位为 bitmap[n - 1]
+        char *s = new char[n + 1];
+        s[n] = '\0'; // 字符串所占空间，由上层调用者负责释放
+        for (Rank i = 0; i < n; i++)
+            s[i] = test(i) ? '1' : '0';
+        return s; // 返回字符串位置
+    }
+    void expand(Rank k)
+    { // 若被访问的 Bitmap[k]已出界，则需扩容
+        if (k < 8 * N)
+            return; // 仍在界内，无需扩容
+        Rank oldN = N;
+        unsigned char *oldM = M;
+        init(2 * k); // 与向量类似，加倍策略
+        memcpy_s(M, N, oldM, oldN);
+        delete[] oldM;
+    } // 原数据转移至新空
 };
