c706a11ea2392e8fcca8575edd99ed202b5e91d5
zgyj4532 <2956918266@qq.com>
2024-11-03 13:48:46 +0800

    init Hashtable

--- lib/Dictionary/Hashtable.cpp
+++ lib/Dictionary/Hashtable.cpp
@@ -2,5 +2,9 @@
 template <typename K, typename V>
 Hashtable<K, V>::Hashtable(int c)
 {
-    M = primeNLT();
+    M = 113;//不小于c的素数
+    N = 0;
+    ht = new Entry<K,V>*[M];
+    memset(ht,0,sizeof(Entry<K,V>*) *M);
+    lazyRemoval = new Bitmap(M);
 }
