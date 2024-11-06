0838a79efc55a0cf78e5b3e24b84cbb861bb16a8
zgyj4532 <2956918266@qq.com>
2024-11-06 12:18:16 +0800

    update generateTable

--- lib/Huff/HuffCode.cpp
+++ lib/Huff/HuffCode.cpp
@@ -1,5 +1,4 @@
 #include "HuffCode.hpp"
-
 void generateCT(Bitmap *code, int length, HuffTable *table, BinNodePosi(HuffChar) v)
 {
     if(IsLeaf(*v))
@@ -8,4 +7,13 @@ void generateCT(Bitmap *code, int length, HuffTable *table, BinNodePosi(HuffChar
     {code->clear(length);generateCT(code,length+1,table,v->lc);}
     if(HasRChild(*v))//right = 1
     {code->set(length);generateCT(code,length+1,table,v->rc);}
+}
+HuffTable* generateTable(HuffTree* tree)
+{
+    HuffTable* table = new HuffTable;
+    Bitmap* code = new Bitmap;
+    generateCT(code,0,table,tree->root());
+    // release(code);
+    code = nullptr;
+    return table;
 }
