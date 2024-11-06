8dca3369d992994845dd83faaef0eccecd947847
zgyj4532 <2956918266@qq.com>
2024-11-06 11:06:23 +0800

    init huffcode

--- lib/Huff/HuffCode.hpp
+++ lib/Huff/HuffCode.hpp
@@ -1,5 +1,10 @@
 #include "Bitmap.hpp"
 #include "Hufftable.hpp"
 #include "BinTree.hpp"
+#include "HuffChar.hpp"
+#include "HuffForest.hpp"
 typedef Bitmap HuffCode;
-// static void generateCT(Bitmap* code,int length,HuffTable* table,)
+static void generateCT(Bitmap* code,int length,HuffTable* table,BinNodePosi(HuffChar));
+HuffTable* generateTable(HuffTree* tree);
+int encode(HuffTable* table,Bitmap* codeString,char* s);
+void decode(HuffTree* tree,Bitmap* code);
