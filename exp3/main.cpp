9c7ee13cb51f1030b60cfe9123fc55bcbd2e57ca
zgyj4532 <2956918266@qq.com>
2024-11-07 10:37:59 +0800

    update exp3 main

--- exp3/main.cpp
+++ exp3/main.cpp
@@ -1,1 +1,18 @@
-#include "BinTree.hpp"
+#include "HuffCode.hpp"
+int main(int argc,char* argv[])
+{   
+    int* freq = statistics(argv[1]);
+    HuffForest* forest = initForest(freq); freq = nullptr;
+    HuffTree* tree = generateTree(forest); forest = nullptr;
+    HuffTable* table = generateTable(tree);
+    for(int i = 2;i<argc;i++)
+    {
+        Bitmap* codeString = new Bitmap;
+        int n = encode(table,codeString,argv[i]);
+        decode(tree,codeString,n);
+        codeString = nullptr;
+    }
+    table = nullptr;
+    tree = nullptr;
+    return 0;
+}
