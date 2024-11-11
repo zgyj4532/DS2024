a912b11c7d4726c9d6728a68d2cd9c91e7929f43
zgyj4532 <2956918266@qq.com>
2024-11-11 14:41:54 +0800

    add 可执行文件main

--- exp3/main.cpp
+++ exp3/main.cpp
@@ -5,26 +5,26 @@
 
 std::unordered_map<char,std::string> huffmanTable;//散装哈夫曼编码表
 
-void travPre_R(BinNodePosi(HuffChar) x ,std::string code = "")
+void travPre_R_show(BinNodePosi(HuffChar) x ,std::string code = "")
 {
 	if (!x) return;
 	// std::cout << x->data.ch << ' ';
-	// travPre_R(x->lc);
-	// travPre_R(x->rc);
+	// travPre_R_show(x->lc);
+	// travPre_R_show(x->rc);
     if(!x->lc && !x->rc) huffmanTable[x->data.ch] = code;
-    if(x->lc) {travPre_R(x->lc,code + '0');}
-    if(x->rc) {travPre_R(x->rc,code + '1');}
+    if(x->lc) {travPre_R_show(x->lc,code + '0');}
+    if(x->rc) {travPre_R_show(x->rc,code + '1');}
 }
 
 int main()
 {   
-	char file[45] = "/home/crayzamb/projects/DS2024/exp3/word.txt";
+	char file[45] = "/home/crayzamb/projects/DS2024/exp3/word.txt";//若选择编译运行请修改file路径（linux端无法导入相对路径
 	unsigned int* freq = statistics(file);//根据样本文件，统计各字符的出现频率
 	HuffForest* forest = initForest(freq); delete freq; //创建Huffman森林
 	HuffTree* tree = generateTree(forest); delete forest;//构造Huffman编码树
     // HuffTable* table - generateTable(tree);
-	std::cout << "哈夫曼编码表为" << std::endl;
-	travPre_R(tree->root());//初始化编码表
+	std::cout << "哈夫曼树前序遍历之后的哈夫曼编码表为：" << std::endl;
+	travPre_R_show(tree->root());//初始化编码表
     for(const auto&i : huffmanTable)
     {
         char data = i.first;
@@ -32,6 +32,7 @@ int main()
         std::cout << data<<": "<< code<<std::endl;
     }
     std::cout<<std::endl;
+    /*bitmap没学，不用*/
     // for(int i = 2;i<argc;i++) 
     // {
     //     Bitmap* codeString = new Bitmap;
