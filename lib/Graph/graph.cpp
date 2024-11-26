1c39dd0a4ea03563416fb24449b03ed5b658e3ae
zgyj4532 <2956918266@qq.com>
2024-11-26 17:57:11 +0800

    add DijkstraPu

--- lib/Graph/graph.cpp
+++ lib/Graph/graph.cpp
@@ -285,4 +285,13 @@ template<typename Tv,typename Te> struct PrimPU
             }
     }
 };
-                                                                 
+template <typename Tv,typename Te> struct DijkstraPU{
+    virtual void operator() (Graph<Tv,Te>* g ,int uk,int v){
+        if(g->status(v) == UNDSICOVERED)
+            if(g->priority(v) > g->priority(uk)+g->weight(uk,v))
+            {
+                g->priority(v) = g->priority(uk)+g->weight(uk,v);
+                g->parent(v) = uk;
+            }
+    }
+};                                                                 
