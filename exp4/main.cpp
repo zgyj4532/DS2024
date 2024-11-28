9c308ee79f3e96aab507d4d8828c08e48efbedf3
zgyj4532 <2956918266@qq.com>
2024-11-28 14:19:21 +0800

    add "G" into g

--- exp4/main.cpp
+++ exp4/main.cpp
@@ -10,25 +10,27 @@ int main()
     g.insert('D');
     g.insert('E');
     g.insert('F');
+    g.insert('G');
 
     // 插入边：顶点与顶点之间的关系
-    g.insert(1, 5, 0, 1); // A -> B
-    g.insert(1, 10, 1, 2); // B -> C
-    g.insert(1, 3, 1, 3); // B -> D
-    g.insert(1, 2, 2, 4); // C -> E
-    g.insert(1, 7, 3, 4); // D -> E
-    g.insert(1, 4, 4, 5); // E -> F
+    g.insert(1, 5, 0, 1); // A -> B 权重 5
+    g.insert(1, 8, 1, 2); // B -> C 权重 8
+    g.insert(1, 3, 1, 3); // B -> D 权重 3
+    g.insert(1, 2, 2, 4); // C -> E 权重 2
+    g.insert(1, 7, 3, 4); // D -> E 权重 7
+    g.insert(1, 4, 4, 5); // E -> F 权重 4
+    g.insert(1, 9, 0, 6); // A -> G 权重 9
     /*
     图g信息为
      A -> B -> C
-          ↓    ↓
-          D -> E -> F
+     ↓     ↓    ↓
+     G     D -> E -> F
 
     */
     cout << "graph g:" << endl;
     cout << "     A -> B -> C" << endl;
-    cout << "          ↓    ↓" << endl;
-    cout << "          D -> E -> F" << endl;
+    cout << "     ↓    ↓    ↓" << endl;
+    cout << "     G    D -> E -> F" << endl;
     // 测试深度优先搜索（DFS）
     std::cout << "DFS Traversal starting from vertex 'A':" << std::endl;
     g.dfs(0); // 从顶点 'A' 开始深度优先搜索（0 表示顶点 A）
@@ -39,8 +41,7 @@ int main()
 
     // 测试拓扑排序搜索（tSort）
     std::cout << "\ntSort Traversal starting from vertex 'A':" << std::endl;
-    g.tSort(0); // 从顶点 'A' 开始广度优先搜索（0 表示顶点 A）
-
+    g.tSort(0); // 从顶点 'A' 开始拓扑排序搜索（0 表示顶点 A）
 
     DijkstraPU<char, int> dijkstraPU;
 
