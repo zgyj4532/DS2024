81eb4ef978407081d583919a1a9ca68e004f621a
zgyj4532 <2956918266@qq.com>
2024-12-16 13:53:34 +0800

    init PQ_ComplHeap

---
+++ lib/PQ/PQ_ComplHeap.hpp
@@ -0,0 +1,16 @@
+#include "Vector.cpp"
+#include "PQ.hpp"
+template <typename T> class PQ_ComplHeap: public PQ<T>,public Vector<T>
+{
+protected:
+    Rank percolateDown(Rank n ,Rank i);//下滤
+    Rank percolateUp(Rank i);//上滤
+    void heapify(Rank n);//建堆
+public:
+    PQ_ComplHeap() { }
+    PQ_ComplHeap(T* A,Rank n) {copyFrom(A,0,n);heapify(n);}//批量构造
+    void insert(T);
+    T getMax();
+    T delMax();
+};
+
