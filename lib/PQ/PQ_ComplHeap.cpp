f3dd86563acf4627eb23925c298b9ea7f10c9e01
zgyj4532 <2956918266@qq.com>
2024-12-20 16:37:25 +0800

    update annotation to help me fix error better

--- lib/PQ/PQ_ComplHeap.cpp
+++ lib/PQ/PQ_ComplHeap.cpp
@@ -4,7 +4,7 @@ Rank PQ_ComplHeap<T>::percolateDown(Rank n, Rank i)
 {
     Rank j;
     while(i!=(j = ProperParent(Vector<T>::_elem,n,i)))
-    {swap(Vector<T>::_elem[i],Vector<T>::_elem[j]);i=j;}
+    {swap(Vector<T>::_elem[i],Vector<T>::_elem[j]);i=j;cout<<i<<"yes"<<endl;}
     return i;
 }
 template <typename T>
@@ -44,10 +44,18 @@ T PQ_ComplHeap<T>::delMax()//删除优先级最高的词条
     return maxElem;
 
 }
+/*
+      3
+   19   14
+  0   3 
+*/
 //基于向量的就地堆排序（不知道放哪了，就放这里了）
 template<typename T> void Vector<T>::heapSort(Rank lo,Rank hi)
 {
     PQ_ComplHeap<T> H(_elem + lo,hi-lo);//建议一个完全二叉堆
     while(!H.empty())//移除已排序的后缀
-        _elem[--hi] = H.delMax();//堆顶与末元素交换后下滤
+        {
+            _elem[--hi] = H.delMax();//堆顶与末元素交换后下滤
+            
+        }
 }
--- lib/PQ/PQ_ComplHeap.hpp
+++ lib/PQ/PQ_ComplHeap.hpp
@@ -1,5 +1,29 @@
 #include "Vector.cpp"
 #include "PQ.hpp"
+#define InHeap(n, i) (((-1) < (i)) && ((i) < (n))) // 判断PQ是否合法
+#define Parent(i) ((i - 1) >> 1)                   // PQ父节点
+#define LastInternal(n) Parent(n - 1)              // PQ末节点父亲
+#define LChild(i) (1 + ((i) << 1))                 // PQ[i]左孩子
+#define RChild(i) ((1 + (i)) << 1)                 // PQ[i]右孩子
+#define ParentValid(i) (i > 0)                     // 判断PQ是否有父亲
+#define LChildValid(n, i) InHeap(n, LChild(i))
+#define RChildValid(n, i) InHeap(n, RChild(i))
+#define Bigger(PQ, i, j) ((PQ[i] < PQ[j]) ? j : i)  // 取最大值
+#define Smaller(PQ, i, j) ((PQ[i] < PQ[j]) ? i : j) // 取较小的值
+/*父子三者中的最大者，什么逆天三目运算符嵌套*/
+#define ProperParent(PQ, n, i) (RChildValid(n, i) ? Bigger(PQ, Bigger(PQ, i, LChild(i)), RChild(i)) : (LChildValid(n, i) ? Bigger(PQ, i, LChild(i)) : i))
+    /*
+    like:
+    if(RChildValid(n, i)) 
+        return Bigger(PQ, Bigger(PQ, i, LChild(i)), RChild(i));
+    else 
+        if(LChildValid(n, i)) 
+        return Bigger(PQ, i, LChild(i)); 
+        else  return i;
+    */
+    
+    // (RChildValid(n, i) ? Smaller(PQ, Smaller(PQ, i, LChild(i)), RChild(i)) : \
+    // (LChildValid(n, i) ? Smaller(PQ, i, LChild(i)) : i))
 template <typename T> class PQ_ComplHeap: public PQ<T>,public Vector<T>
 {
 protected:
